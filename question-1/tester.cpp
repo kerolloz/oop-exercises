c82d6eb9ae7898ac5038afd54aa2be867359a71c
kerollos <kerolloz@yahoo.com>
2019-03-28 16:05:45 +0200

    add question-1 as an example

--- question-1/tester.cpp
+++ question-1/tester.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 #include "solution.cpp"
-#include "checker.hpp"
 
 #define endl '\n'
 
@@ -9,8 +8,8 @@ using namespace std;
 int main (){
     Point p1(5, 5);
     Point p2(7, 7);
-    p1.getDistance(p2);
-
-
-    return 0;
+    if(p1.getDistance(p2) == int(sqrt(8)))
+        return 0;
+    else 
+        return 1;
 }
