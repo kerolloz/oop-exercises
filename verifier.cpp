896a26876a0bc01e1c371716b889b5dddb64d411
kerollos <kerolloz@yahoo.com>
2019-03-28 16:07:09 +0200

    implement verifier

--- verifier.cpp
+++ verifier.cpp
@@ -3,8 +3,24 @@
 
 using namespace std;
 
-int main ()
+int main (int argc, char* argv[])
 {
-      
+    if(argc == 2){
+        // the second parameter is the question number to be verified
+        int questionNumber = stoi(string(argv[1]));
+        string questionDir = "question-" + string(argv[1]) + "/";
+        Checker c(questionDir + "solution.cpp", questionDir + "tester.cpp");
+        if(Compilation::SUCCESS == c.compile() && Test::SUCCESS == c.test())
+            cout << "Correct Answer\n";
+        else
+            cout << "Wrong Answer.\nTry Harder!\n";
+        
+            
+    }else{
+        cout << "Please add the question number to be provide as a parameter\n";
+        return 1;
+    }
+
+    return 0;
 } 
 
