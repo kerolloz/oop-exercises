11ab4e05d718c51f2ea26afc6f177bd939318637
kerollos <kerolloz@yahoo.com>
2019-03-28 16:07:26 +0200

    add checker header file

--- checker.hpp
+++ checker.hpp
@@ -1,18 +1,26 @@
-#include "compiler.hpp"
+#include "flags.hpp"
+#include <string>
 
 class Checker{
     private:
-        string solution;
-        string tester;
+        std::string solution;
+        std::string tester;
         
     public:
-        Checker(string sol, string test){
+        Checker(std::string sol, std::string test){
             this->solution = sol;
             this->tester = test;
         }
         Compilation compile(){
-            string command = "g++ " + tester;
+            std::string command = "g++ " + this->tester + " -o test";
             int compilationState = system(command.c_str());
             return (compilationState)? Compilation::FAILED : Compilation::SUCCESS ;
         }
+        Test test(){
+            std::string command = "./test";
+            int returnValue = system(command.c_str());
+            return (returnValue == 0)? Test::SUCCESS : Test::FAILED; // success
+                
+        }
+
 };
