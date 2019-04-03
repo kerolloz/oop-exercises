c33e3749669ace953ad28bb4d285f932dc9791c9
Mohamed <elraghy8@gmail.com>
2019-04-03 15:24:07 +0200

    Tester to the problem

--- exercises/question-2/tester.cpp
+++ exercises/question-2/tester.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 #include "solution.cpp"
+
+using namespace std;
+
+int main() {
+
+    Time t1(1, 30, 25), t2(2, 22, 10);
+    Time t3;
+
+    t3.Add(t1, t2);
+    
+    if(t3.hours == 4 && t3.min == 30 && !t3.second) return 0;
+    else return 1;
+}
