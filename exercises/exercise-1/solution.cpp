5b6a6b61c04a1890ffd9c7ece32b8f82d0367438
Anh Quan <62094489+nhoc20170861@users.noreply.github.com>
2021-09-03 19:13:59 +0700

    it's my solution

--- exercises/exercise-1/solution.cpp
+++ exercises/exercise-1/solution.cpp
@@ -1,7 +1,15 @@
 
 class Point
 {
-	
-    
+private:
+    int x, y;
+public:
+    Point(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+    double getDistance(Point& P2) {
+        return sqrt((this->x - P2.x) * (this->x - P2.x) + (this->y - P2.y) * (this->y - P2.y));
+    }	  
 };
 
